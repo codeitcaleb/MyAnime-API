5c64fcf526e1c35cf7e0d5cd01deab79d15c73fe
Caleb Kavanaugh <calebck@hotmail.com>
2020-03-13 14:08:34 -0400

    Added Route to Root.js for MyAnimeShow component.

--- myanimecal-frontend/src/Root.js
+++ myanimecal-frontend/src/Root.js
@@ -3,18 +3,20 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import App from "./App";
-import MyAnime from './containers/MyAnime'
+import MyAnime from './containers/MyAnimeContainer'
 import AnimeCalendarContainer from "./containers/AnimeCalendarContainer";
 import AnimeShow from "./containers/AnimeShow";
-import AnimeResults from "./components/AnimeList"
+import MyAnimeShow from "./components/MyAnimeShow"
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Navbar />
       <Switch>
         <Route exact path="/" component={App} />
-        <Route exact path="/calendar" component={AnimeCalendarContainer} /> 
+        <Route exact path="/calendar" component={AnimeCalendarContainer} />
         <Route exact path="/myanime" component={MyAnime} />
+        <Route exact path="/myanime/:id" component={MyAnimeShow} />
         <Route exact path="/anime/:mal_id" component={AnimeShow} />
       </Switch>
     </Router>
