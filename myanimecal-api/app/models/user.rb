a4dcabc7f26168c8f69b59f233d3709a3b30e6e9
Caleb Kavanaugh <calebck@hotmail.com>
2020-03-15 14:46:17 -0400

    Renamed Directories to match project naming.

--- myanimecal-api/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    has_many :user_schedules
-    has_many :user_anime_reviews
-    has_many :animes, through: :user_schedules
-
-    # validates :name, presence: true
-    # validates :email, uniqueness: true
-    # has_secure_password
-end
